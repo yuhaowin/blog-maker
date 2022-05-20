d1c400d206ee3cb56a0c5e77dec6eea747dffeef
yuhaowin <yuhao.coding@gmail.com>
2022-05-20 19:39:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,52 @@
+
+## Install
+Compiling from source code:  
+```bash
+go build -o cider main.go
+```
+
+## Generate your site through Github Action(Recommending)
+1. Folk this project.
+2. Gernerate a SSH key for Github Action commit:
+```
+ssh-keygen -t rsa -b 4096 -C $(git config user.email) -f gh-pages -N ""
+```
+It will create two files: `gh-pages` and `gh-pages.pub`.   
+Copy all contents of the `gh-pages.pub` into `Github/Setting/SSH and GPG keys/Add SSH Key.`  
+![ssh_public_key](rs/images/ssh_pub.png)   
+Copy all contents of the `gh-pages` into `this project(your folkd)/Settings/Secrets/New repository secret`. The name must be **ACTIONS_DEPLOY_KEY**.  
+![ssh_private_key](rs/images/ssh_private.jpg)  
+3. Create a repository named `your_name.github.io`.    
+4. Copy the repository SSH url into `this project(your folkd)/Settings/Secrets/New repository secret`. The name must be **DEPLOY_REPOSITORY**.  
+![ssh_repository_url](rs/images/repository_url.jpg)
+
+All done, happy writing.   
+
+You can empty all md files in `content` and all images files in `rs/images`, those are my demo contents. When you want to reference an image in your article, you should put the file in `rs/images`. It will be synced to `your_name.github.io` in which all md files are generated.
+
+**Notice:** The CNAME file uploading and domain binding and other settings must be configured with yourself.  
+
+### Showing your site on local
+`./cider s`
+
+### Classify your articles
+If you want to classify your articles into different groups, you should create a subfolder into `content`, and then add the folder link into `templates/partials/_nav.html.tpl`. For example, if you have written an article about English learning, and want to create a English Learning group in your website.
+```bash
+mkdir content/el
+mv your_md_file content/el/
+```
+
+Add `el` into `templates/partials/_nav.html.tpl`
+```html
+[ <a class="nav-btn" href="/el/">English</a> ]
+```
+
+### Insert an image into your article
+Create a `images` folder in public, copy your picture into the folder, write the relative path in your article.
+```markdown
+![your_picture](/images/your_picture.jpg)
+```
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/leyafo/cider/render"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+var(
+	outputFolder  = flag.String("o", "public", "")
+	contentFolder = "content"
+	templatePath  = "templates"
+	metaPath      = ".meta"
+	draftFolder   = "draft"
+	indexTemplate = "index.html.tpl"
+	postTemplate  = "post.html.tpl"
+	addr          = ":8080"
+)
+
+func cwdPath(subPath ...string)string{
+	// using the function
+	workDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, p := range subPath{
+		workDir = filepath.Join(workDir, p)
+	}
+	return workDir
+}
+
+
+func printHelp(){
+	fmt.Println("./cider -o path\n", "render all MD files(in content folder) to path, default is ./public" )
+	fmt.Println("./cider s \n", "render all MD files and then start a HTTP server to exhibit your github pages")
+	fmt.Println("./cider d \n", "start a HTTP server and render contents in draft folder." )
+}
+
+func main(){
+	log.SetFlags(log.Lshortfile)
+
+	if len(os.Args) == 2{
+		s := viewingServer{}
+		s.PostList = make(render.RenderList)
+		s.PostTemplate = render.GetTemplate(cwdPath(templatePath),postTemplate)
+		s.IndexTemplate = render.GetTemplate(cwdPath(templatePath),indexTemplate)
+		switch os.Args[1] {
+		case "s":
+			s.ContentDir=cwdPath(contentFolder)
+			s.PostList.UpdateRenderList(s.ContentDir)
+			log.Println("Starting HTTP file server at http://localhost:8080/")
+			log.Fatal(http.ListenAndServe(addr, http.HandlerFunc(s.viewingServer)))
+			return
+		case "d":
+			s.ContentDir=cwdPath(draftFolder)
+			s.PostList.UpdateRenderList(s.ContentDir)
+			log.Println("Starting HTTP file server at http://localhost:8080/")
+			log.Fatal(http.ListenAndServe(addr, http.HandlerFunc(s.viewingServer)))
+			return
+		case "h":
+			printHelp()
+			return
+		default:
+			break
+		}
+	}
+	flag.Parse()
+	outputPath, _ := filepath.Abs(*outputFolder)
+	render.Render(cwdPath(templatePath), cwdPath(contentFolder), filepath.Join(outputPath, metaPath), outputPath)
+}
+
+type viewingServer struct {
+	ContentDir string
+	PostList      render.RenderList
+	PostTemplate  *template.Template;
+	IndexTemplate *template.Template
+}
+
+func (s *viewingServer) viewingServer(w http.ResponseWriter, r *http.Request)  {
+	var err error
+	p := strings.TrimSpace(r.URL.Path)
+	if p[len(p)-1] == '/'{
+		err = render.GenerateListWithPath(s.IndexTemplate, s.PostList, p, w)
+	}else if strings.Index(p, "/rs/images") == 0{
+		f, err := os.Open(cwdPath(p))
+		if err != nil{
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		io.Copy(w, f)
+	}else{
+		if p, ok := s.PostList[p]; ok{
+			err = render.GeneratePostOut(s.PostTemplate, p.GetMDPath(s.ContentDir), w)
+		}else{
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+	}
+	if err != nil{
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}else{
+		w.WriteHeader(http.StatusOK)
+	}
+}
